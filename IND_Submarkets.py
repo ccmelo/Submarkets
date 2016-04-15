d6b2d2a17740e4fe3d31653e6ea6b71bab95f0d7
ccmelo <carlmellow@gmail.com>
2016-04-14 21:36:36 -0400

    initial commit

---
+++ IND_Submarkets.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 14 21:17:20 2016
+
+@author: cmelo
+"""
+
+import pandas 
+neighbors=pandas.read_csv("/Users/cmelo/Google Drive/Costar work/IND Submarkets/LOSA_Neighbors.csv", header=0, index_col=0)
+print neighbors 
