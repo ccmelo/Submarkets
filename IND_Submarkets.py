d22d4c37d7849149b1dd22baea8649147b31edbb
Carlota Melo <cmelo@us.costar.local>
2016-04-18 17:32:07 -0400

    add iter lopp through all neighbors in all submarkets; add new_submarkets

--- IND_Submarkets.py
+++ IND_Submarkets.py
@@ -16,50 +16,45 @@ import json
 
 #WORK FILE PATHS
 neighbors_file="P:\Work in Progress\Carlota Melo\IND Submarkets\Submarkets\LOSA_Neighbors.csv"
-data_file="P:\Work in Progress\Carlota Melo\IND Submarkets\Submarkets\LOSAsubmarketdata.csv"
+submarkets_file="P:\Work in Progress\Carlota Melo\IND Submarkets\Submarkets\SubmarketList.csv"
 
+#reads in csv neighbors data as a pandas dataframe 
 neighbors_raw=pandas.read_csv(neighbors_file, header=0, index_col=0)
+#neighbors data structure initialized 
 neighbors=defaultdict(tuple)
 neighbor_dict={}
-templist=[]
 previous_index=""
-
-i=0
-
-for index, row in neighbors_raw.iterrows(): 
-    if index!=previous_index and previous_index!="": 
-        templist=[]
-    #I'm only trying to combine submarkets less than 10million SF in size; with other submarkets less than 15millionSF in size 
-    if float(row['src_Inventory'])<10000000 and float(row['nbr_Inventory'])<15000000:
-        print index,"this is what Index I'm on" 
-        neighbor_dict.clear()
-        neighbor_dict[row['nbr_LOGCode']]=(int(row['nbr_Inventory']), int(row['nbr_Avg_Buiding_Size']))
-        print neighbor_dict," this is dict I just created as neighbordict"
-        templist.append(neighbor_dict.copy())
-        print templist
-        neighbors[index]=(templist,row['src_Inventory'])
-        previous_index=index
-        
-data_as_dict = json.loads(json.dumps(neighbors))
-print(data_as_dict['LOG-LOSA-01'])
+#new submarkets keep track of whether submarket has been combined and the "code" of new submarket
+new_submarkets=pandas.read_csv(submarkets_file, header=0, index_col=0)
+new_submarkets['combine']=0
+new_submarkets['new_code']=""
+#print new_submarkets.head()
+#converts raw data to more useful structure
 """
 neighbors is a dictionary where key is submarket code and value is a TUPLE where [0] is a list of all neighbors, where each neighbor is represented as 
 a dictionary where key is the neighbor's submarket code and value is a tuple containing (nbr_Invesntory, nbr_Avg_BLDG_size) and [1] is src_inventory 
-
+"""
     
+for index, row in neighbors_raw.iterrows(): 
+    if index!=previous_index: 
+        templist=[]
+    #I'm only trying to combine submarkets less than 10million SF in size; with other submarkets less than 15millionSF in size 
+    if float(row['src_Inventory'])<10000000 and float(row['nbr_Inventory'])<15000000:
+        neighbor_dict.clear()
+        neighbor_dict[row['nbr_LOGCode']]=(int(row['nbr_Inventory']), int(row['nbr_Avg_Buiding_Size']))
+        #append a COPY of just created dict, not the actual dictionary 
+        templist.append(neighbor_dict.copy())
+        neighbors[index]=(templist,int(row['src_Inventory']))
+        previous_index=index
 
+#below line converts dictionary to json and back to dict, for priting help, but I don't think I need this    
+#data_as_dict = json.loads(json.dumps(neighbors))
 
+#test line 
+#print neighbors['LOG-LOSA-13']
 
-
-
-
-
-
+#iterate through all neighbors in all submarkets 
 for submarket in neighbors.keys():
-    for neighbor in neighbors[submarket]:
-        print neighbor
-        try:
-            print submarket_data.loc[neighbor]['N_prop']
-        except:
-            pass
-            """
+    for neighbor in neighbors[submarket][0]:
+        #test line: if submarket=='LOG-LOSA-01':
+            print neighbor, '\n'
